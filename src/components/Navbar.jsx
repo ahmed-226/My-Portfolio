9e5141f8e89651ae80624bf4bc77a2358e568566
ahmed mhmoud abdelaal <ahmedmhmouad41@gmail.com>
2024-09-24 17:23:18 +0300

    final commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,7 @@ import {
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import Logo from '../assets/Logo.png'
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
 
@@ -22,27 +23,67 @@ const Navbar = () => {
       </div>
       <div className='hidden md:flex'>
         <ul className='hidden md:flex'>
-          <li>Home</li>
-          <li>Skill</li>
-          <li>About</li>
-          <li>Work</li>
-          <li>Contact</li>
+          <li>
+            <Link to='home' smooth={true} duration={800}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to='skills' smooth={true} duration={800}>
+              Skill
+            </Link>
+          </li>
+          <li>
+            <Link to='About' smooth={true} duration={800}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to='work' smooth={true} duration={800}>
+              Work
+            </Link>
+          </li>
+          <li>
+            <Link to='contact' smooth={true} duration={800}>
+              Contact
+            </Link>
+          </li>
         </ul>
       </div>
 
       {/* Hamgurger  */}
-      <div onClick={handleClick} className='md:hidden z-10'>
+      <div onClick={handleClick} className='md:hidden z-10 mr-2'>
         {!nav ? <FaBars size={30} /> : <FaTimes size={30} />}
       </div>
 
       {/* mobile   */}
 
       <ul className={!nav ? 'hidden' : ' absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-        <li className='py-6 text-4xl'>Home</li>
-        <li className='py-6 text-4xl'>Skill</li>
-        <li className='py-6 text-4xl'>About</li>
-        <li className='py-6 text-4xl'>Work</li>
-        <li className='py-6 text-4xl'>Contact</li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='home' smooth={true} duration={800}>
+            Home
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='skills' smooth={true} duration={800}>
+            Skill
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='About' smooth={true} duration={800}>
+            About
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='work' smooth={true} duration={800}>
+            Work
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='contact' smooth={true} duration={800}>
+            Contact
+          </Link>
+        </li>
       </ul>
 
       {/* soical icons  */}
@@ -51,12 +92,12 @@ const Navbar = () => {
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[0px] duration-300 bg-[#6fc2b0]  '>
             <a href="/" className='flex justify-between items-center w-full text-gray-300'>
-              Email <HiOutlineMail  size={30} className='ml-2' />
+              Email <HiOutlineMail size={30} className='ml-2' />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[0px] duration-300 bg-[#565f69]'>
             <a href="/" className='flex justify-between items-center w-full text-gray-300'>
-              Facebook  <BsFillPersonLinesFill size={30} className='ml-2'/>
+              Facebook  <BsFillPersonLinesFill size={30} className='ml-2' />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[0px] duration-300  bg-[#333333]'>
@@ -66,7 +107,7 @@ const Navbar = () => {
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[0px] duration-300 bg-[#4d92d2] '>
             <a href="/" className='flex justify-between items-center w-full text-gray-300'>
-              LinkedIN <FaLinkedin size={30}  className='ml-2'/>
+              LinkedIN <FaLinkedin size={30} className='ml-2' />
             </a>
           </li>
         </ul>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,9 +13,9 @@ const Skills = () => {
     <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
     {/* Container */}
     <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div>
+        <div className='mb-5'>
             <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
-            <p className='py-4 text-2xl'>// These are the technologies I've worked with</p>
+            <p className='py-4 text-2xl'>These are the technologies I've worked with</p>
         </div>
 
         <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
